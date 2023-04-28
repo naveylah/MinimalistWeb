cd0be6f98f4f22e8c764ca829b99f89d2efcb97d
Nabilah Daud <48626338+naveylah@users.noreply.github.com>
2023-04-28 17:30:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+import "./styles.css";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+const burger = document.querySelector("nav svg");
+
+burger.addEventListener("click", () => {
+  if (burger.classList.contains("active")) {
+    gsap.to(".links", { x: "100%" });
+    gsap.to(".line", { stroke: "white" });
+    // stop scrolling after burger
+    gsap.set("body", { overflow: "auto" });
+    gsap.set("body", { overflowX: "hidden" });
+  } else {
+    gsap.to(".links", { x: "0%" });
+    gsap.to(".line", { stroke: "black" });
+    // not colour bcs svg
+    gsap.fromTo(
+      ".links a",
+      { opacity: 0, y: 0 },
+      { opacity: 1, y: 20, delay: 0.25, stagger: 0.25 }
+    );
+    gsap.set("body", { overflow: "hidden" });
+  }
+  burger.classList.toggle("active");
+});
+
+const videos = gsap.utils.toArray(".video");
+gsap.set(videos, { opacity: 0 });
+
+videos.forEach((video) => {
+  ScrollTrigger.create({
+    trigger: video,
+    start: "top center",
+    end: "bottom center",
+    markers: true,
+
+    onEnter: () => {
+      gsap.to(video, { opacity: 1 });
+      video.play();
+    },
+    onEnterBack: () => video.play(),
+    onLeave: () => video.pause(),
+    onLeaveBack: () => video.pause(),
+  });
+});
